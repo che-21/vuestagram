e5fb5a7161e9ec9c54d85e2f91016e737b3b8091
che-21 <cieletoile623@gmail.com>
2024-02-12 20:47:08 +0900

    01.레이아웃 만들기 ~ 02. style 속성 데이터바인딩

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="(a, i) in insta" :key="i">
+    <Post :insta="insta" :i="i"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post';
+
+export default {
+    components : {
+        Post,
+    },
+    props : {
+      insta : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div class="profile" :style="{ backgroundImage : `url(${insta[i].userImage})` }"></div>
+      <span class="profile-name">{{ insta[i].name }}</span>
+    </div>
+    <div class="post-body" :style="{ backgroundImage : `url(${insta[i].postImage})` }"></div>
+    <div class="post-content">
+      <p>{{ insta[i].likes }} Likes</p>
+      <p><strong>{{ insta[i].name }}</strong> {{ insta[i].content }}</p>
+      <p class="date">{{ insta[i].date }}</p>
+    </div>
+</div> 
+</template>
+
+<script>
+
+export default {
+    props : {
+      insta : Array,
+      i : Number,
+    }
+}
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://picsum.photos/100?random=0");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://picsum.photos/600?random=0");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
